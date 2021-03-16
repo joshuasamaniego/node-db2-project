4c4d68de4b66b457cd6dc7ece50341fb052fd901
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-03-16 16:39:01 -0600

    meets mvp

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
-const express = require("express")
+const express = require("express");
+const server = express();
+const carsRouter = require('./cars/cars-router');
+const { logger } = require('./cars/cars-middleware');
 
-const server = express()
+server.use(express.json());
 
-// DO YOUR MAGIC
+server.use('/api/cars', logger, carsRouter);
+
+// catch all error response
+server.use((err, req, res, next) => {//eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+        custom: 'You hit the catch-all error message on server.js. You didnt hit a valid endpoint'
+    })
+})
 
 module.exports = server
